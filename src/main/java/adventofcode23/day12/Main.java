886239ef65d3d54fcaad00f32cf89c40d3eb9e8a
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-12-24 10:21:00 +0100

    chore: add day12 current progress

--- src/main/java/adventofcode23/day12/Main.java
+++ src/main/java/adventofcode23/day12/Main.java
@@ -1,6 +1,9 @@
 package adventofcode23.day12;
 
+import adventofcode23.lib.ParserResult;
 import adventofcode23.lib.ResourceReader;
+
+import java.util.List;
 
 
 public class Main {
@@ -11,8 +14,15 @@ public class Main {
         ResourceReader reader = new ResourceReader();
         String fileName = "input/day12/input.txt";
         RecordParser parser = new RecordParser();
-        reader.parseContents(fileName, parser);
 
+        List<ParserResult> results = reader.parseContents(fileName, parser);
+
+        long sum = 0;
+        for (ParserResult res: results) {
+            sum += (long) res.getValue();
+        }
+
+        System.out.println("The sum of the counts is " + sum);
 
     }
 }
--- src/main/java/adventofcode23/day12/RecordParser.java
+++ src/main/java/adventofcode23/day12/RecordParser.java
@@ -8,6 +8,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class RecordParser extends Parser {
 
@@ -21,22 +22,25 @@ public class RecordParser extends Parser {
             return res;
         }
 
-        int sum = compute(line);
+        long sum = compute(line);
 
         res.setValue(sum);
+
+        // # ### ##
 
         return res;
     }
 
-    public int compute(String line) {
+    public long compute(String line) {
 
-        int sum = 0;
+        long sum = 0;
 
         String[] parts = line.split(" ");
 
         List<String> ignoreCode = new ArrayList<>();
 
         String[] codeParts = parts[0].split("\\.");
+
         String codeString = parts[0];
         List<String> codes = new ArrayList<>();
         for (String s : codeParts) {
@@ -50,78 +54,201 @@ public class RecordParser extends Parser {
             numbers.add(Integer.valueOf(s));
         }
 
-        sum = probe(codes, numbers.stream().mapToInt(i -> i ).toArray());
+        //sum = probe(codes, numbers.stream().mapToInt(i -> i ).toArray());
 
+        sum = probe(parts[0], numbers.stream().mapToInt(i -> i ).toArray());
 
         return sum;
     }
 
-    public int probe(List<String> codes, int[] numbers) {
+    public long probe(List<String> codes, int[] numbers) {
+        return 0;
+    }
 
-        LinkedList<String> list = new LinkedList<>();
-        for (int i = 0; i < numbers.length; i++) {
-            list.add("#".repeat(numbers[i]));
-        }
+    public long probe(String codes, int[] numbers) {
 
-        int sum = 0;
-        int start = 0;
-        for (String code: codes) {
-            int cmp = 0;
-            List<Integer> partitions = new ArrayList<>();
-            System.out.println("starting at " + start);
-            for (int i = start; i < numbers.length; i++) {
-                start = i;
-                if (cmp + numbers[i] + (i > 0 && i <= numbers.length - 1 ? 1 : 0)  <= code.length()) {
-                    cmp += numbers[i] + i;
-                    partitions.add(numbers[i]);
+        long sum = 0;
+        long product = 1;
+        int numberCursor = 0;
+        System.out.println(Arrays.toString(numbers));
+
+        String code = codes;
+
+       // for (int w = 0; w < codes.size(); w++) {
+
+            int hashPos = 0;
+            int codeLen = code.length();
+            int objs = 0;
+            List<Integer> numParts = new ArrayList<>();
+
+            int complete = 0;
+
+            for (int i = 0; i < numbers.length; i++) {
+                numParts.add(numbers[i]);
+            }
+
+            System.out.println("Probing " + numberCursor + "/" + " with " + Arrays.toString(numbers) + " for " + code + " and " + codeLen);
+            for (int i = numberCursor; i < numbers.length; i++) {
+
+                // check if number can be skipped if a pattern of the same length
+                // appears in code
+            //    int hashes = sumSymbols(code, "#");
+              //  code = code.replace("#", "");
+
+                // normalize the code string based on the required length
+               if (complete + numbers[i] + numParts.size() <= codeLen) {
+                    complete += numbers[i];
+                    if (numbers[i] > 1) {
+                        objs += (numbers[i] -1);
+                    }
+                    numParts.add(numbers[i]);
+                 //   numberCursor++;
                 } else {
                     break;
                 }
             }
-            System.out.println(partitions.size() + " possible partitions for " + code + " (" + Arrays.toString(partitions.toArray()) + ")");
 
-            int[] parts = partitions.stream().mapToInt(i->i).toArray();
+            System.out.println("Finding pattern for "+ code + " " + Arrays.toString(numParts.stream().mapToInt(i->i).toArray()));
+            code = findPattern(code, numParts);
+            System.out.println("New Code: "+ code + " " + Arrays.toString(numParts.stream().mapToInt(i->i).toArray()));
 
-            List<int[]> groups = new ArrayList<>();
 
+          //  codeLen -= (objs + Math.max(0, numParts.size() - 1));
 
+            int[] parts = numParts.stream().mapToInt(i->i).toArray();
+            codeLen = code.length();
             for (int i = 0; i < parts.length; i++) {
-
-                int tail = 0;
-                for (int j = 0; j < i; j++) {
-                    tail += numbers[j];
+                if (parts[i] > 1) {
+                    codeLen -= (parts[i] - 1);
                 }
+            }
+            codeLen -= Math.max(0, parts.length - 1);
 
-                int head = 1;
-                for (int j = i + 1; j < parts.length; j++) {
-                    head += numbers[j];
-                }
+            System.out.println("Codelen " + codeLen + " (" + code + ") " + Arrays.toString(parts) + " (numberCursor: " + numberCursor + ")");
 
-                groups.add(
-                    new int[]{tail, (code.length()) - head
-                    }
-                );
-                System.out.println("added " + tail + " " + (code.length() - head) + " codeLength: " + code.length());
+
+            int set = codeLen;
+            int objects = parts.length;
+            if (set == 0 || objects == 0) {
+                System.err.println("set or objects == 0");
+            } else {
+                long partialResult = fac(set) / (fac(set - objects) * fac(objects));
+                System.out.println("Computing combinations for a set of " + set + " and " + objects + " objects: " + partialResult);
+                System.out.println(" --");
+                product *= partialResult;
+                sum += product;
             }
 
-            int i = 0;
-            int partialResult = 0;
-            System.out.println(Arrays.toString(parts));
-            for (int[] group: groups) {
-                int spots = (group[1]) - group[0] + 1;
-                int length = parts[i];
-                System.out.println("partial result for " + (spots) +" " + parts[i]);
-                partialResult += spots - (groups.size() - 1 )- parts[i] + 1;
-                i++;
-            }
-            sum += partialResult ;
-        }
+
+        //}
 
         return sum;
 
     }
 
 
+    public String findPattern(String code, List<Integer> numParts) {
+
+        int[] numbers = numParts.stream().mapToInt(i->i).toArray();
+
+        String newCode = "";
+
+        if (code.contains("#")) {
+
+            int textCursor = 0;
+
+            for (int j = 0; j < numbers.length; j++) {
+                String cmp = "1".repeat(numbers[j]);
+                String middle = "0" + cmp + "0";
+                String start = cmp + "0";
+                String end = "0" +  cmp;
+
+                String base = code.replace("?" , "0");
+                base = base.replace("#" , "1");
+                cmp = padRightZeros("1".repeat(numbers[j]), base.length());
+
+                int max = 0;
+                int pos = 0;
+                for (int c = 0; c < base.length(); c++) {
+                    cmp = padRightZeros("0".repeat(c) + "1".repeat(numbers[j]), base.length());
+                    int startC = c;
+                    int endC = Math.min(base.length(), c + cmp.length());
+
+                    int user = Integer.parseUnsignedInt(
+                            base.substring(startC, endC),
+                            2
+                    );
+                    int mask = Integer.parseUnsignedInt(cmp, 2);
+
+                    if ((user & mask) > max) {
+                        pos = c;
+                    }
+                    max = Math.max(max, user & mask);
+                }
+
+                System.out.println("Remaining: " + remaining(j + 1, numbers));
+                if (max > 0 && remaining(j + 1, numbers) <= code.length() - (pos + 1)) {
+                    if (pos + numbers[j] < base.length()) {
+                        if (base.charAt(pos + numbers[j]) == '1') {
+                            continue;
+                        }
+                    }
+
+                    System.out.print("Current code: "+ code+ "; Position: " + pos +"; textCursor: " + textCursor);
+
+                    for (int i = 0; i < numParts.size(); i++) {
+                        if (numParts.get(i).equals(numbers[j])) {
+                            numParts.remove(i);
+                            break;
+                        }
+                    }
+                    char leadingChar = pos > 0 ? code.charAt(pos - 1) : '_';
+
+                    boolean trimLeft = pos > 0 && code.charAt(pos - 1) == '?' ;
+                    boolean trimRight =  pos + numbers[j] < code.length() && code.charAt(pos + numbers[j] ) == '?' ;
+
+                    boolean trimLeftAndRight = pos > 0 && pos + numbers[j] - 1 < code.length() && code.charAt(pos) == '#' && code.charAt(pos + numbers[j] - 1) == '#';
+
+                    if (trimLeftAndRight) {
+                        //throw new RuntimeException();
+                    }
+
+                    //pos = leadingChar == '?' ? pos - 1 : pos;
+                    code = code.substring(
+                            textCursor,
+                            textCursor + pos
+                    )
+                            + code.substring(Math.min(code.length() , textCursor + pos + numbers[j] + (trimLeftAndRight ? 1 : 0) ));
+                    //textCursor += cmp.length();
+                    if (trimLeftAndRight && !code.isEmpty()) {
+                        code = code.substring(1, code.length());
+                    } else if ((trimLeft || trimRight) && !code.isEmpty()) {
+                        code = code.substring(1);
+                    }
+
+                    System.out.println(" ; code: " + code + "; numParts: " + Arrays.toString(numParts.stream().mapToInt(i->i).toArray()));
+
+
+                }
+            }
+
+        }
+
+        return code;
+    }
+
+    private Long fac(int n) {
+        return LongStream.rangeClosed(1, n)
+             .reduce(1, (long x, long y) -> x * y);
+    }
+
+    public int remaining(int index, int[] numbers) {
+        int sum = 0;
+        for (int i = index; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
 
     public void oldProbe(List<String> codes, int[] numbers) {
 
@@ -265,7 +392,26 @@ public class RecordParser extends Parser {
     }
 
 
+    public String padLeftZeros(String inputString, int length) {
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - inputString.length()) {
+            sb.append('0');
+        }
+        sb.append(inputString);
 
+        return sb.toString();
+    }
+
+    public String padRightZeros(String inputString, int length) {
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        inputString += "0";
+        return padRightZeros(inputString, length);
+    }
     private void swap(int pos1, int pos2, char[] c)
     {
         char temp = c[pos1];
