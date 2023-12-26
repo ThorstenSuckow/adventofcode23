b2c4467b2c660c6080a060f4a81fa63a5bd4d769
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-12-26 21:48:17 +0100

    feat: add solution to part two of day 18

--- src/main/java/adventofcode23/day18/Main.java
+++ src/main/java/adventofcode23/day18/Main.java
@@ -12,15 +12,18 @@ public class Main {
         ResourceReader reader = new ResourceReader();
         String fileName = "input/day18/input.txt";
         DiggerParser parser;
+        List<ParserResult> results;
+
         parser = new DiggerParser();
-
-        List<ParserResult> results = reader.parseContents(fileName, parser);
-
+        results = reader.parseContents(fileName, parser);
         double sum = (double) results.get(0).getValue();
-
         System.out.println("The dig plan can hold a total of " +((int) sum) + " cubic meters.");
 
+        parser = new DiggerParser(true);
+        results = reader.parseContents(fileName, parser);
+        sum = (double) results.get(0).getValue();
 
+        System.out.println("The lagoon can hold a total of " +((long) sum) + " cubic meters of lava.");
     }
 
 }
